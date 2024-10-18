4168b24cc5e206e0458a6d4afd3efaccc214f0dc
Wallace Oliveira <wall_h@hotmail.com>
2024-10-18 13:50:55 -0300

    Configurando DB

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTOs;
+using MinimalApi.Inflaestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(builder.Configuration.GetConnectionString("mysql"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));
+});
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
